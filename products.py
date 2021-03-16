ff01fbc512f540f0005b7f5793307e0cb8043243
pedromaironi <pedrom_t@hotmail.com>
2021-03-16 01:54:03 +0100

    [Bot] The products command has been added and configured

---
+++ products.py
@@ -0,0 +1,65 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import *
+markup = InlineKeyboardMarkup()
+
+def gen_markup():
+    x = count_products()
+    print(str(x))
+    y = showInfoProducts()
+    markup.row_width = 1
+    markup.row_height = 2
+    for k in y:
+        print(k['name'] + "\n" + "id" + k['id'])
+        markup_add(k['name'], k['id'])
+    return markup
+
+def markup_add(name, name_callback):
+    markup.add(InlineKeyboardButton(name, callback_data=name_callback))
+
+@bot.callback_query_handler(func=lambda call: 'Products')
+def callback_query(call):
+    # print(call.message.json['reply_markup']['inline_keyboard'][1]['text'])
+    data = {}
+    name_product = ""
+    if call.data == "1":
+        for x in call.message.json['reply_markup']['inline_keyboard']:
+            data = x
+            print(data)
+            for x in data:
+                print(x)
+                if x['callback_data'] == "1":
+                    name_product = x['text']
+        print(name_product)
+        bot.answer_callback_query(call.id, name_product)
+    elif call.data == "2":
+        data = {}
+        name_product = ""
+        if call.data == "2":
+            for x in call.message.json['reply_markup']['inline_keyboard']:
+                data = x
+                print(data)
+                for x in data:
+                    print(x)
+                    if x['callback_data'] == "2":
+                        name_product = x['text']
+            print(name_product)
+            bot.answer_callback_query(call.id, name_product)
+    elif call.data == "3":
+        data = {}
+        name_product = ""
+        if call.data == "3":
+            for x in call.message.json['reply_markup']['inline_keyboard']:
+                data = x
+                print(data)
+                for x in data:
+                    print(x)
+                    if x['callback_data'] == "3":
+                        name_product = x['text']
+            print(name_product)
+            bot.answer_callback_query(call.id, name_product)
+
+@bot.message_handler(func=lambda message: True)
+def message_handler(message):
+    bot.send_message(message.chat.id, "Productos", reply_markup=gen_markup())
+
